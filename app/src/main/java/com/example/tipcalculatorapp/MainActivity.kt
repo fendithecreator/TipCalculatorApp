0f1147aa5242e29a7716c4c32fb3fab1b1ed6999
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-03-01 22:45:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tipcalculatorapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.tipcalculatorapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tipcalculatorapp.ui.theme.TipCalculatorAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TipCalculatorApp {  }
+        }
+    }
+}
+
+@Composable
+fun TipCalculatorApp(content: @Composable () -> Unit) {
+    TipCalculatorAppTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Column(modifier = Modifier) {
+                content()
+            }
+        }
+    }
+}
